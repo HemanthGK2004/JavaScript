006bbd48ef62e794258bd21a598ef2a3b962c6ca
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-03-02 18:50:51 +0000

    saved

---
+++ Basics/datatypes.js
@@ -0,0 +1,18 @@
+let example = "Hemanth"
+console.log(example);
+let n = 10;
+let i =0
+let sum = 0;
+
+// function fact(n){
+//     if(n==0||n==1)return 1;
+//     return n * fact(n-1);
+// }
+// console.log(fact(4))
+let a =10;
+for(let i=1;i<=a;i++){
+    for(let j =1;j<=10;j++){
+        console.log(i*j)
+    }
+    console.log("\n")
+}
