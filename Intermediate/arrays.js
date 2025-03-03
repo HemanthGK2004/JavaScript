9f785f741036d5b3adb463a5af1ff9ba43525802
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-03-03 14:57:27 +0000

    saved

---
+++ Intermediate/arrays.js
@@ -0,0 +1,17 @@
+// const array = [1,2,3,5,6,7,8,9]
+// let sum = 0;
+// let k = 1;
+// for(let i =0;i<8;i++){
+//     if(array[i]==k){
+//         console.log(i);
+//     }
+// }
+
+//Methods
+
+const array = new Array(1,2,3,4,6,6)
+array.push(10)
+array.pop()
+array.unshift(9)
+console.log(array);
+
