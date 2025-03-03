f17821d4425668a1979d0b52cbeb7f7898c35dd9
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-03-03 14:34:19 +0000

    saved

---
+++ Basics/num and math.js
@@ -0,0 +1,29 @@
+// const num =100
+// const balanace = new Number(5000)
+// console.log(num)
+// console.log(balanace)
+
+// console.log(balanace.toString())
+// console.log(balanace.toFixed(2))
+
+// const otherNum = 26.955
+// console.log(otherNum.toPrecision(3))
+
+// const number = 1000000
+// console.log(number.toLocaleString('en-IN'))
+
+
+// Maths
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(23.666))
+// console.log(Math.ceil(4.66));
+
+console.log(Math.floor(Math.random()*10))+1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
--- Basics/strings.js
+++ Basics/strings.js
@@ -2,3 +2,8 @@ let userName= "Hemanth";
 let useWife = "sanajana"
 console.log(userName + useWife)
 console.log(`hello my name ${userName} and i am good boy`)
+
+const gameName  = new String('Cricket')
+console.log(gameName[1])
+console.log(gameName.__proto__)
+console.log(gameName.length)
