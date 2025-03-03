18b50b71cac065ccc03941135d1ae59ae321881a
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-03-03 14:47:16 +0000

    saved

---
+++ Basics/dates and time.js
@@ -0,0 +1,16 @@
+// let muyDate = new Date()
+// console.log(muyDate.toString());
+// console.log(muyDate.toDateString());
+// console.log(muyDate.toLocaleString());
+
+
+// let myDate = new Date(2025,2,3)
+// console.log(myDate.toDateString());
+
+
+// let myTimeStamp =Date.now()
+// console.log(myTimeStamp);
+// console.log(myDate.getTime())
+
+// let date = new Date()
+// console.log(date.getDay());
