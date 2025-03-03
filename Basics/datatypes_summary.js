d7cfe385cdd57227886d5a54ee3a1cdfb115ec9c
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-03-03 13:53:25 +0000

    saved

---
+++ Basics/datatypes_summary.js
@@ -0,0 +1,15 @@
+// primitive datatypes
+//7types : strings , number , boolean, null , undefined , symbol , BigInt , 
+
+const id = String('123')
+const anotherId = String('123')
+console.log(id===anotherId)
+
+//reference or non primitive datatypes
+ //Arrays , objects , functions
+
+ const heroes = ['hemanth','sanjana',"gdgd"]
+ const Sumname = function (a,b){
+    return a+b;
+ }
+console.log(Sumname(5,10))
