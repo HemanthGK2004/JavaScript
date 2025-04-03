2550c76001e29fbf625f921b19d0c03c515b0981
Hemanth G K <138641801+HemanthGK2004@users.noreply.github.com>
2025-04-03 08:36:14 +0000

    Added

---
+++ Intermediate/higherOrder.js
@@ -0,0 +1,26 @@
+// const coding = ["js","cpp","py",'java']
+// coding.forEach(function(val){
+//     console.log(val);    
+// })
+
+// coding.forEach( (item,index,arr)=>{
+// console.log(item,index,arr);
+// })
+
+const myName =[
+    {
+        Name:"Hemanth",
+        SRN:"R22EF082"
+    },
+    {
+        Name:"Indhushree",
+        SRN:"R22EF083"
+    },
+    {
+        Name:"Sanjana"
+    },
+]
+myName.forEach((item)=>{
+    console.log(item.Name);
+    
+})
